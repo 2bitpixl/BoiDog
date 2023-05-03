eaf40e681f8941a5836f7c1f34b154f2f06a645b
topengl <tobias.pingel@googlemail.com>
2023-05-03 15:13:20 +0200

    init

---
+++ MyToken.sol
@@ -0,0 +1,14 @@
+/*
+BOI DOG is wants to be the most helpful dog of the web.
+The meme coin relates to the borzoi meme trend "let me do it for you".
+*/
+// define solidity version
+pragma solidity ^0.8.19;
+
+// define smart contract
+// base it the open-zeppelin library from github
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+// with the "is" keyword all the definitions of zeppelin library ERC 20 are available in MyToken
+contract MyToken is ERC20 {
+
+}
